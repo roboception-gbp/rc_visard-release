b97f164cfbc48338372106d9c24478ed4acdc171
Felix Ruess <felix.ruess@gmail.com>
2019-05-20 17:31:57 +0200

    Revert "Trimmed the branch to only the rc_visard sub directory"
    This reverts commit aed4b0746ead976596f7ab5e376b850ed20c6c14.

---
+++ rc_hand_eye_calibration_client/src/rc_hand_eye_calibration_client_node.cpp
@@ -0,0 +1,35 @@
+#include "rest_hand_eye_calibration_client.h"
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  std::string name = "rc_hand_eye_calibration";
+  ros::init(argc, argv, name);
+
+  ros::NodeHandle pnh("~");
+  std::string host;
+  pnh.param("host", host, host);
+
+  if (host.empty()) {
+    ROS_FATAL("No host set! Please set the parameter 'host'!");
+    return 1;
+  }
+
+  std::unique_ptr<CalibrationWrapper> calib_wrapper;
+
+  try
+  {
+    // instantiate wrapper and advertise services
+    calib_wrapper.reset(new CalibrationWrapper(host, pnh));
+  }
+  catch (const std::exception &ex)
+  {
+    ROS_FATAL("Client could not be created due to an error: %s", ex.what());
+    return 1;
+  }
+
+  ROS_INFO_STREAM("Hand eye calibration node started for host: " << host);
+
+  // start ros node
+  ros::spin();
+}
