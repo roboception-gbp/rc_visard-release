5341426981c2629170eecec173a1a4c80829c5b0
Felix Ruess <felix.ruess@roboception.de>
2020-05-13 21:23:10 +0200

    Rebase from 'release/kinetic/rc_pick_client'

--- opt/cpr/include/cpr/session.h
+++ opt/cpr/include/cpr/session.h
@@ -9,6 +9,7 @@
 #include "cpr/cookies.h"
 #include "cpr/cprtypes.h"
 #include "cpr/digest.h"
+#include "cpr/low_speed.h"
 #include "cpr/max_redirects.h"
 #include "cpr/multipart.h"
 #include "cpr/parameters.h"
@@ -16,8 +17,13 @@
 #include "cpr/proxies.h"
 #include "cpr/response.h"
 #include "cpr/timeout.h"
+#include "cpr/connect_timeout.h"
 #include "cpr/low_speed.h"
 #include "cpr/ssl_options.h"
+#include "cpr/timeout.h"
+#include "cpr/user_agent.h"
+#include "cpr/session.h"
+#include "cpr/verbose.h"
 
 namespace cpr {
 
@@ -31,8 +37,10 @@ class Session {
     void SetParameters(Parameters&& parameters);
     void SetHeader(const Header& header);
     void SetTimeout(const Timeout& timeout);
+    void SetConnectTimeout(const ConnectTimeout& timeout);
     void SetAuth(const Authentication& auth);
     void SetDigest(const Digest& auth);
+    void SetUserAgent(const UserAgent& ua);
     void SetPayload(Payload&& payload);
     void SetPayload(const Payload& payload);
     void SetProxies(Proxies&& proxies);
@@ -53,8 +61,10 @@ class Session {
     void SetOption(Parameters&& parameters);
     void SetOption(const Header& header);
     void SetOption(const Timeout& timeout);
+    void SetOption(const ConnectTimeout& timeout);
     void SetOption(const Authentication& auth);
     void SetOption(const Digest& auth);
+    void SetOption(const UserAgent& ua);
     void SetOption(Payload&& payload);
     void SetOption(const Payload& payload);
     void SetOption(Proxies&& proxies);
@@ -68,6 +78,7 @@ class Session {
     void SetOption(const Body& body);
     void SetOption(const LowSpeed& low_speed);
     void SetOption(const VerifySsl& verify);
+    void SetOption(const Verbose& verbose);
 
     Response Delete();
     Response Get();
